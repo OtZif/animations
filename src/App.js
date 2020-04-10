aeda99d3d0476d21fb55bea1bdca0409fd474b3e
Zarif <zarif.otambekov@saltpepper.ru>
2020-04-10 11:26:14 +0300

    fin

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { PureComponent } from 'react';
+import circle from './circles';
+import buttons from './buttons';
+import Ball from './Ball';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends PureComponent {
+  state = {
+    newCircle: circle,
+  };
+
+  getRandomValue = () => {
+    return Math.floor(Math.random() * 256).toString(16);
+  };
+
+  getRandomColor = () => {
+    return `#${this.getRandomValue()}${this.getRandomValue()}${this.getRandomValue()}`;
+  };
+
+  handleClick = (circleNumber, animation) => () => {
+    this.setState({
+      newCircle: this.state.newCircle.map((el) => {
+        if (el.id === circleNumber) {
+          if (animation === 'color') {
+            return {
+              ...el,
+              color: this.getRandomColor(),
+            };
+          }
+          return {
+            ...el,
+            animation: animation,
+          };
+        }
+        return {
+          ...el,
+          animation: '',
+        };
+      }),
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          {buttons.map((el) => {
+            return (
+              <button
+                key={el.id}
+                onClick={this.handleClick(el.circleNumber, el.animation)}
+              >
+                {el.animation}
+              </button>
+            );
+          })}
+        </div>
+        {this.state.newCircle.map((el) => {
+          return <Ball key={el.id} animation={el.animation} color={el.color} />;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
